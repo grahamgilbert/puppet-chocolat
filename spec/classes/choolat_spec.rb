304f5f41db8505e401336628656d409310ba6168
Graham Gilbert <graham@grahamgilbert.com>
2013-04-22 14:54:13 +0100

    Tests.

--- spec/classes/choolat_spec.rb
+++ spec/classes/choolat_spec.rb
@@ -4,7 +4,7 @@ describe 'chocolat' do
   it do
     should contain_package('Chocolat').with({
       :provider => 'compressed_app',
-      :source   => 'http://files.fileability.net/Chocolat_1.4.3.zip',
+      :source   => 'http://files.fileability.net/Chocolat_1.6.2.zip',
     })
   end
 end
